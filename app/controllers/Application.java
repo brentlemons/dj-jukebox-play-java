ac8a00cfc39f5160e4ac31168619a347c5a69f2f
Brent Lemons <blemons@credera.com>
2014-02-22 23:58:05 -0600

    stubbed artist to models

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,19 @@
 package controllers;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import models.Artist;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import static play.libs.Json.toJson;
+
 import play.*;
+import play.data.Form;
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
 import play.mvc.*;
 
 import views.html.*;
@@ -8,7 +21,26 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(Form.form(Artist.class)));
+    }
+
+    @Transactional
+    public static Result addArtist() {
+        Form<Artist> form = Form.form(Artist.class).bindFromRequest();
+        Artist bar = form.get();
+        JPA.em().persist(bar);
+        return redirect(controllers.routes.Application.index());
+    }
+
+    @Transactional(readOnly = true)
+    public static Result listArtists() {
+        CriteriaBuilder cb = JPA.em().getCriteriaBuilder();
+        CriteriaQuery<Artist> cq = cb.createQuery(Artist.class);
+        Root<Artist> root = cq.from(Artist.class);
+        CriteriaQuery<Artist> all = cq.select(root);
+        TypedQuery<Artist> allQuery = JPA.em().createQuery(all);
+        JsonNode jsonNodes = toJson(allQuery.getResultList());
+        return ok(jsonNodes);
     }
 
 }
